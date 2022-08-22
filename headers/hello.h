cb420b0e854589ab2e951b2f83ed07794b63e9a1
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2022-08-22 22:18:58 +0530

    [Procheta] Add. files

---
+++ headers/hello.h
@@ -0,0 +1,4 @@
+#ifndef HelloWorld
+#define HelloWorld
+void HelloWorld();
+#endif
---
+++ source/hello.cpp
@@ -0,0 +1,7 @@
+#include 
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+}
